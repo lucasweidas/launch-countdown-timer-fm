ea6477910ec5bb275c42952b70f7648cd805842e
lucasweidas <lucasweidas@gmail.com>
2023-09-02 20:00:42 -0300

    timer animation progress

--- components/Countdown.tsx
+++ components/Countdown.tsx
@@ -26,10 +26,31 @@ export function Countdown() {
 function Timer({ time, label }: TimerProps) {
   return (
     <div className="flex flex-col gap-3 items-center md:gap-6">
-      <div className="flex items-center justify-center w-timer h-timer bg-indigo-700 rounded-md md:w-timer-lg md:h-timer-lg shadow-timer md:shadow-timer-lg">
-        <span className="font-bold text-rose-300 text-3xl md:text-7xl">
-          {time.toString().padStart(2, '0')}
-        </span>
+      <div className="relative w-timer h-timer md:w-timer-lg md:h-timer-lg shadow-timer md:shadow-timer-lg">
+        {/* Current Timer: TOP */}
+        <div className="w-full h-full flex items-center justify-center bg-indigo-700 rounded-md absolute timer-top flip-top-to-bottom">
+          <span className="font-bold text-rose-300 text-3xl md:text-7xl">
+            {time.toString().padStart(2, '0')}
+          </span>
+        </div>
+        {/* Current Timer: BOTTOM */}
+        <div className="w-full h-full flex items-center justify-center bg-indigo-700 rounded-md absolute timer-bottom">
+          <span className="font-bold text-rose-300 text-3xl md:text-7xl">
+            {time.toString().padStart(2, '0')}
+          </span>
+        </div>
+        {/* New Timer: TOP */}
+        <div className="w-full h-full flex items-center justify-center bg-indigo-700 rounded-md absolute timer-top -z-[1]">
+          <span className="font-bold text-rose-300 text-3xl md:text-7xl">
+            {(time + 1).toString().padStart(2, '0')}
+          </span>
+        </div>
+        {/* New Timer: BOTTOM */}
+        <div className="w-full h-full flex items-center justify-center bg-indigo-700 rounded-md absolute timer-bottom -z-[1] flip-top-to-bottom">
+          <span className="font-bold text-rose-300 text-3xl md:text-7xl">
+            {(time + 1).toString().padStart(2, '0')}
+          </span>
+        </div>
       </div>
       <span className="uppercase text-indigo-400 text-[0.5rem] font-bold tracking-[0.3em] md:text-sm">
         {label}
