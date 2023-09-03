83a710b86a0569d5af9a100d67876d044c269885
lucasweidas <lucasweidas@gmail.com>
2023-09-03 20:03:12 -0300

    add countdown logic

--- components/Countdown.tsx
+++ components/Countdown.tsx
@@ -1,60 +1,94 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useRef } from 'react';
 
 interface TimerProps {
-  time: number;
+  initialTime: string;
+  attr: string;
   label: string;
 }
 
 export function Countdown() {
-  const [days, setDays] = useState(8);
-  const [hours, setHours] = useState(23);
-  const [minutes, setMinutes] = useState(55);
-  const [seconds, setSeconds] = useState(41);
+  const countToDate = useRef(new Date().setHours(new Date().getHours() + 216));
+  const previousTimeBetweenDates = useRef(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const currentDate = new Date();
+      const timeBetweenDates = Math.ceil(
+        (countToDate.current - currentDate.getTime()) / 1000
+      );
+      flipAllCards(timeBetweenDates);
+
+      previousTimeBetweenDates.current = timeBetweenDates;
+    }, 250);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
     <div className="grid grid-cols-2 gap-4 xs:grid-cols-4 md:gap-8">
-      <Timer time={days} label="days" />
-      <Timer time={hours} label="hours" />
-      <Timer time={minutes} label="minutes" />
-      <Timer time={seconds} label="seconds" />
+      <Timer initialTime="8" attr="days" label="days" />
+      <Timer initialTime="23" attr="hours" label="hours" />
+      <Timer initialTime="55" attr="minutes" label="minutes" />
+      <Timer initialTime="41" attr="seconds" label="seconds" />
     </div>
   );
 }
 
-function Timer({ time, label }: TimerProps) {
+function Timer({ initialTime, attr, label }: TimerProps) {
   return (
     <div className="flex flex-col gap-3 items-center md:gap-6">
-      <div className="relative w-timer h-timer md:w-timer-lg md:h-timer-lg shadow-timer md:shadow-timer-lg">
-        {/* Current Timer: TOP */}
-        <div className="w-full h-full flex items-center justify-center bg-indigo-700 rounded-md absolute timer-top flip-top-to-bottom">
-          <span className="font-bold text-rose-300 text-3xl md:text-7xl">
-            {time.toString().padStart(2, '0')}
-          </span>
-        </div>
-        {/* Current Timer: BOTTOM */}
-        <div className="w-full h-full flex items-center justify-center bg-indigo-700 rounded-md absolute timer-bottom">
-          <span className="font-bold text-rose-300 text-3xl md:text-7xl">
-            {time.toString().padStart(2, '0')}
-          </span>
-        </div>
-        {/* New Timer: TOP */}
-        <div className="w-full h-full flex items-center justify-center bg-indigo-700 rounded-md absolute timer-top -z-[1]">
-          <span className="font-bold text-rose-300 text-3xl md:text-7xl">
-            {(time + 1).toString().padStart(2, '0')}
-          </span>
-        </div>
-        {/* New Timer: BOTTOM */}
-        <div className="w-full h-full flex items-center justify-center bg-indigo-700 rounded-md absolute timer-bottom -z-[1] flip-top-to-bottom">
-          <span className="font-bold text-rose-300 text-3xl md:text-7xl">
-            {(time + 1).toString().padStart(2, '0')}
-          </span>
-        </div>
+      <div className="flip-card" data-timer={attr}>
+        <span className="top">{initialTime.padStart(2, '0')}</span>
+        <span className="bottom">{initialTime.padStart(2, '0')}</span>
       </div>
       <span className="uppercase text-indigo-400 text-[0.5rem] font-bold tracking-[0.3em] md:text-sm">
         {label}
       </span>
     </div>
   );
+}
+
+function flipAllCards(time: number) {
+  const seconds = time % 60;
+  const minutes = Math.floor(time / 60) % 60;
+  const hours = Math.floor((time / 3600) % 24);
+  const days = Math.floor((time / 86400) % 30);
+
+  flip(document.querySelector('[data-timer="days"]')!, days);
+  flip(document.querySelector('[data-timer="hours"]')!, hours);
+  flip(document.querySelector('[data-timer="minutes"]')!, minutes);
+  flip(document.querySelector('[data-timer="seconds"]')!, seconds);
+}
+
+function flip(flipCard: HTMLElement, newNumber: number) {
+  const topHalf = flipCard.querySelector('.top')!;
+  const startNumber = parseInt(topHalf.textContent!);
+  if (newNumber === startNumber) return;
+
+  const bottomHalf = flipCard.querySelector('.bottom')!;
+  const topFlip = document.createElement('div');
+  topFlip.classList.add('top-flip');
+  const bottomFlip = document.createElement('div');
+  bottomFlip.classList.add('bottom-flip');
+
+  topHalf.textContent = startNumber.toString().padStart(2, '0');
+  bottomHalf.textContent = startNumber.toString().padStart(2, '0');
+  topFlip.textContent = startNumber.toString().padStart(2, '0');
+  bottomFlip.textContent = newNumber.toString().padStart(2, '0');
+
+  topFlip.addEventListener('animationstart', e => {
+    topHalf.textContent = newNumber.toString().padStart(2, '0');
+  });
+  topFlip.addEventListener('animationend', e => {
+    topFlip.remove();
+  });
+  bottomFlip.addEventListener('animationend', e => {
+    bottomHalf.textContent = newNumber.toString().padStart(2, '0');
+    bottomFlip.remove();
+  });
+  flipCard.append(topFlip, bottomFlip);
 }
