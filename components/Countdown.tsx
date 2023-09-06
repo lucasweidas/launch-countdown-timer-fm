f3b02b221bffb87f534d67b396932e17f75a8ed9
lucasweidas <lucasweidas@gmail.com>
2023-09-06 14:25:42 -0300

    add useCountdown hook

--- components/Countdown.tsx
+++ components/Countdown.tsx
@@ -45,37 +45,8 @@ const flipBottomVariants: Variants = {
 };
 
 export function Countdown() {
-  const countToDate = useRef(new Date().setHours(new Date().getHours() + 216));
-  const previousTimeBetweenDates = useRef(0);
-  const [seconds, setSeconds] = useState(12);
-  const [minutes, setMinutes] = useState(34);
-  const [hours, setHours] = useState(56);
-  const [days, setDays] = useState(78);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const currentDate = new Date();
-      const timeBetweenDates = Math.ceil(
-        (countToDate.current - currentDate.getTime()) / 1000
-      );
-
-      const newSeconds = timeBetweenDates % 60;
-      const newMinutes = Math.floor(timeBetweenDates / 60) % 60;
-      const newHours = Math.floor(timeBetweenDates / 3600) % 24;
-      const newDays = Math.floor(timeBetweenDates / 86400) % 30;
-
-      setSeconds(newSeconds);
-      setMinutes(newMinutes);
-      setHours(newHours);
-      setDays(newDays);
-
-      previousTimeBetweenDates.current = timeBetweenDates;
-    }, 250);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
+  const endAt = useRef(new Date(Date.now() + 777_600_000));
+  const { days, hours, minutes, seconds } = useCountdown(endAt.current);
 
   return (
     <div className="grid grid-cols-2 gap-4 xs:grid-cols-4 md:gap-8">
@@ -90,13 +61,13 @@ export function Countdown() {
 const Timer = memo(function Timer({ time, label }: TimerProps) {
   return (
     <div className="flex flex-col gap-3 items-center md:gap-6">
-      <div className="w-timer md:w-timer-lg h-timer md:h-timer-lg relative shadow-timer md:shadow-timer-lg flex flex-col">
+      <div className="w-timer md:w-timer-lg h-timer md:h-timer-lg relative shadow-timer md:shadow-timer-lg flex flex-col rounded-md">
         <AnimatePresence initial={false} mode="popLayout">
           <motion.div
             key={crypto.randomUUID()}
             variants={flipTopVariants}
             exit="exit"
-            className="bg-indigo-850 w-full h-2/4 rounded-md overflow-hidden relative flex justify-center"
+            className="bg-indigo-800 w-full h-2/4 rounded-md overflow-hidden relative flex justify-center"
           >
             <span className="text-rose-300 font-bold text-4xl md:text-7xl leading-none absolute top-3.5 md:top-[2.125rem]">
               {time.toString().padStart(2, '0')}
@@ -122,3 +93,43 @@ const Timer = memo(function Timer({ time, label }: TimerProps) {
     </div>
   );
 });
+
+function useCountdown(endAt: Date) {
+  const previousTimeBetweenDates = useRef(0);
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [days, setDays] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const currentDate = new Date();
+      const timeBetweenDates = Math.ceil(
+        (endAt.getTime() - currentDate.getTime()) / 1000
+      );
+
+      const newSeconds = timeBetweenDates % 60;
+      const newMinutes = Math.floor(timeBetweenDates / 60) % 60;
+      const newHours = Math.floor(timeBetweenDates / 3600) % 24;
+      const newDays = Math.floor(timeBetweenDates / 86400) % 30;
+
+      setDays(newDays);
+      setHours(newHours);
+      setMinutes(newMinutes);
+      setSeconds(newSeconds);
+
+      previousTimeBetweenDates.current = timeBetweenDates;
+    }, 250);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [endAt]);
+
+  return {
+    seconds,
+    minutes,
+    hours,
+    days,
+  };
+}
